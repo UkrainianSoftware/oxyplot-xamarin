cf9bf2e7f7f91a5acdb62af60fa7d4458e6c44e6
dodikk <dodikk88.reg@gmail.com>
2021-06-17 19:58:49 +0300

    XM-939: [proto] [tracker] updating point lookup logic to ignore Y coord

--- Source/OxyPlot.Xamarin.Forms/Series/AreaSeriesForDragTracker.cs
+++ Source/OxyPlot.Xamarin.Forms/Series/AreaSeriesForDragTracker.cs
@@ -18,6 +18,8 @@ namespace OxyPlot
         // * https://github.com/oxyplot/oxyplot/blob/075d1b3808946e0661c0544af248dfdc3a898ebc/Source/OxyPlot/Series/XYAxisSeries.cs#L310
         // * https://github.com/oxyplot/oxyplot/blob/075d1b3808946e0661c0544af248dfdc3a898ebc/Source/OxyPlot/Series/XYAxisSeries.cs#L214
         // * https://github.com/oxyplot/oxyplot/blob/075d1b3808946e0661c0544af248dfdc3a898ebc/Source/OxyPlot/Series/XYAxisSeries.cs#L227
+        // * https://github.com/oxyplot/oxyplot/blob/075d1b3808946e0661c0544af248dfdc3a898ebc/Source/OxyPlot/Rendering/ScreenPointHelper.cs#L74
+        // * https://github.com/oxyplot/oxyplot/blob/075d1b3808946e0661c0544af248dfdc3a898ebc/Source/OxyPlot/Rendering/ScreenPointHelper.cs#L106
         // -
 
 
@@ -53,9 +55,12 @@ namespace OxyPlot
             TrackerHitResult result;
             if (result1 != null && result2 != null)
             {
-                double dist1 = result1.Position.DistanceTo(point);
-                double dist2 = result2.Position.DistanceTo(point);
-                result = dist1 < dist2 ? result1 : result2;
+                double dist1 = Math.Abs(result1.Position.X - point.X); // result1.Position.DistanceTo(point);
+                double dist2 = Math.Abs(result2.Position.X - point.X); // result2.Position.DistanceTo(point);
+
+                result = (dist1 < dist2)
+                    ? result1
+                    : result2;
             }
             else
             {
@@ -105,8 +110,10 @@ namespace OxyPlot
                     continue;
                 }
 
-                var sp = this.Transform(p.x, p.y);
-                double d2 = (sp - point).LengthSquared;
+                var sp = this.Transform(p.X, p.Y);
+                double d2 =
+                    (sp.X - point.X)
+                  * (sp.X - point.X);
 
                 if (d2 < minimumDistance)
                 {
@@ -144,7 +151,10 @@ namespace OxyPlot
         /// <param name="point">The point.</param>
         /// <returns>A tracker hit result if a point was found.</returns>
         /// <remarks>The Text property of the result will not be set, since the formatting depends on the various series.</remarks>
-        protected TrackerHitResult GetNearestInterpolatedPointInternalX(List<DataPoint> points, int startIdx, ScreenPoint point)
+        protected TrackerHitResult GetNearestInterpolatedPointInternalX(
+            List<DataPoint> points,
+            int startIdx,
+            ScreenPoint point)
         {
             if (this.XAxis == null || this.YAxis == null || points == null)
             {
@@ -170,6 +180,9 @@ namespace OxyPlot
                 var sp2 = this.Transform(p2);
 
                 // Find the nearest point on the line segment.
+                // ---
+                // !!! TODO: maybe updatete this method to ignore Y
+                // -
                 var spl = ScreenPointHelper.FindPointOnLine(point, sp1, sp2);
 
                 if (ScreenPoint.IsUndefined(spl))
@@ -178,12 +191,17 @@ namespace OxyPlot
                     continue;
                 }
 
-                double l2 = (point - spl).LengthSquared;
+                double l2 =
+                    (point.X - spl.X)
+                  * (point.X - spl.X);
 
                 if (l2 < minimumDistance)
                 {
-                    double segmentLength = (sp2 - sp1).Length;
-                    double u = segmentLength > 0 ? (spl - sp1).Length / segmentLength : 0;
+                    double segmentLength = Math.Abs(sp2.X - sp1.X);
+                    double u = (segmentLength > 0)
+                        ? Math.Abs(spl.X - sp1.X) / segmentLength
+                        : 0;
+
                     dpn = this.InverseTransform(spl);
                     spn = spl;
                     minimumDistance = l2;
@@ -236,6 +254,70 @@ namespace OxyPlot
             ScreenPoint point)
         {
             return this.GetNearestInterpolatedPointInternalX(points, 0, point);
+        }
+
+
+
+
+        /// <summary>
+        /// Finds the point on line.
+        /// </summary>
+        /// <param name="p">The point.</param>
+        /// <param name="p1">The first point on the line.</param>
+        /// <param name="p2">The second point on the line.</param>
+        /// <returns>The nearest point on the line.</returns>
+        /// <remarks>See <a href="http://paulbourke.net/geometry/pointlineplane/">Bourke</a>.</remarks>
+        public static ScreenPoint FindPointOnLine(
+            ScreenPoint p,
+            ScreenPoint p1,
+            ScreenPoint p2)
+        {
+            double dx = p2.X - p1.X;
+            double dy = p2.Y - p1.Y;
+            double u = FindPositionOnLine(p, p1, p2);
+
+            if (double.IsNaN(u))
+            {
+                u = 0;
+            }
+
+            if (u < 0)
+            {
+                u = 0;
+            }
+
+            if (u > 1)
+            {
+                u = 1;
+            }
+
+            return new ScreenPoint(p1.X + (u * dx), p1.Y + (u * dy));
+        }
+
+        /// <summary>
+        /// Finds the nearest point on line.
+        /// </summary>
+        /// <param name="p">The point.</param>
+        /// <param name="p1">The start point on the line.</param>
+        /// <param name="p2">The end point on the line.</param>
+        /// <returns>The relative position of the nearest point.</returns>
+        /// <remarks>See <a href="http://paulbourke.net/geometry/pointlineplane/">Bourke</a>.</remarks>
+        public static double FindPositionOnLine(
+            ScreenPoint p,
+            ScreenPoint p1,
+            ScreenPoint p2)
+        {
+            double dx = p2.X - p1.X;
+            double dy = p2.Y - p1.Y;
+            double u1 = ((p.X - p1.X) * dx) + ((p.Y - p1.Y) * dy);
+            double u2 = (dx * dx) + (dy * dy);
+
+            if (u2 < 1e-6)
+            {
+                return double.NaN;
+            }
+
+            return u1 / u2;
         }
     }
 }
