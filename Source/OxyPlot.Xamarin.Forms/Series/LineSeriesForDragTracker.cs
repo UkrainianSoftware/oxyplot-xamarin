f48be127a7c623a3bfd9c3f1648a099232ce4596
dodikk <dodikk88.reg@gmail.com>
2021-06-30 19:56:03 +0300

    XM-948: [proto] [tracker] inherit LineSeries to ignore Y coordinate

---
+++ Source/OxyPlot.Xamarin.Forms/Series/LineSeriesForDragTracker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OxyPlot.Series;
+
+
+namespace OxyPlot
+{
+    public class LineSeriesForDragTracker: LineSeries
+    {
+        public LineSeriesForDragTracker()
+        {
+        }
+    }
+}
